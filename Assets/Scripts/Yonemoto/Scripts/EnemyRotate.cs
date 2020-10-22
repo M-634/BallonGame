8a16c8f451707c62a54cc856e05a4764fdf6a82b
sasuke-0723 <kouki072817@gmail.com>
2020-10-22 17:00:19 +0900

    400~500mのステージ

---
+++ Assets/Scripts/Yonemoto/Scripts/EnemyRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotate : MonoBehaviour
+{
+    public float m_speed;
+    public float radius;
+
+    private Vector3 pos;
+    private float x, z;
+
+    void Start()
+    {
+        // positionをオブジェクトの初期値に設定する
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        x = radius * Mathf.Sin(Time.time * m_speed);
+        z = radius * Mathf.Cos(Time.time * m_speed);
+        transform.position = new Vector3(x + pos.x, pos.y, z + pos.z);
+    }
+}
