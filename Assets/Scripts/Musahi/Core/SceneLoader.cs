e027bc7269a0438e7dcfecdc63880e34167af47b
teruteru634 <miyanagateru634@gmail.com>
2020-10-26 17:35:51 +0900

    Load処理を書いてる

---
+++ Assets/Scripts/Musahi/Core/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+///非同期処理を使ったシーンロード 
+/// </summary>
+public class SceneLoader : SingletonMonoBehavior<SceneLoader>
+{
+    private IEnumerator m_currentLoadCorutine;
+
+    public void LoadAddtiveWithCallback(string sceneName,UnityAction callback = null)
+    {
+        m_currentLoadCorutine = LoadAddtiveWithCallbackCorutine(sceneName, callback);
+        StartCoroutine(m_currentLoadCorutine);
+    }
+
+    private IEnumerator LoadAddtiveWithCallbackCorutine(string sceneName,UnityAction callback = null)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        async.allowSceneActivation = true;
+
+        while (async.progress < 0.99f)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        callback?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Musahi/Test/Test_Btn_Musashi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_Btn_Musashi : MonoBehaviour
+{
+    [SerializeField] Image m_fadeImage;
+    [SerializeField] float m_fadeTime;
+    [SerializeField] string m_loadSceneName;
+
+
+    public void OnClickBtn()
+    {
+        m_fadeImage.SetAlpha(1f);
+        SceneLoader.Instance.LoadAddtiveWithCallback(m_loadSceneName,() =>StartCoroutine(m_fadeImage.Fadeout(m_fadeTime)));
+
+    }
+}
