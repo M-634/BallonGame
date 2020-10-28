5c88bc0ebdea86f79930abde0480f87fd877d91d
sorasemashi <sorasemashi1@gmail.com>
2020-10-28 16:16:10 +0900

    スクリプト追加

---
+++ Assets/Scripts/Seya/DoTweenTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DG.Tweening;
+public class DoTweenTest : MonoBehaviour
+{
+    [SerializeField] bool swing;
+    [SerializeField] bool flexibility;
+    void Start()
+    {
+        if (swing)
+        {
+            DOTween.Sequence()
+                .Append(this.transform.DOMoveX(25, 3f).SetRelative())
+                .Join(this.transform.DORotate(new Vector3(0, 180, 0), 3f))
+                .Append(this.transform.DOMoveX(-25, 3f).SetRelative())
+                .Join(this.transform.DORotate(new Vector3(0, 360, 0), 3f))
+                .SetLoops(-1)
+                .Play();
+        }
+        else if (flexibility)
+        {
+            DOTween.Sequence()
+                .Append(this.transform.DOScale(new Vector3(15, 5, 3), 3f).SetRelative())
+                .Append(this.transform.DOScale(new Vector3(-15, -5, -3), 3f).SetRelative())
+                .SetLoops(-1)
+                .Play();
+        }
+    }
+}
--- Assets/Scripts/Seya/Seya.cs
+++ Assets/Scripts/Seya/Seya.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class Seya : MonoBehaviour
 {    // 瀬谷です
-    [SerializeField] bool isLeft;
     private const float radius = 11.0f;
     private float angle = 0.0f;
     [SerializeField] float xSetPos;
@@ -14,18 +14,8 @@ public class Seya : MonoBehaviour
 
     void FixedUpdate()
     {
-
-
-        if (isLeft)
-        {
-            objPos = new Vector3(radius * Mathf.Cos(angle) + xSetPos, ySetPos, zSetPos+ radius * Mathf.Sin(angle));
-            angle += 2.0f * Mathf.PI / 50.0f;
-        }
-        else
-        {
-            objPos = new Vector3(radius * Mathf.Cos(angle) + xSetPos, ySetPos, zSetPos + radius * Mathf.Sin(angle));
-            angle -= 2.0f * Mathf.PI / 50.0f;
-        }
+        objPos = new Vector3(radius * Mathf.Cos(angle) + xSetPos, ySetPos, zSetPos + radius * Mathf.Sin(angle));
+        angle -= 2.0f * Mathf.PI / 50.0f;
         transform.position = objPos;
     }
 }
