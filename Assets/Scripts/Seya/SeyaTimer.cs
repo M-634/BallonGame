6acffecb65c558dc1a1020872d3604a2cea270a0
sorasemashi <sorasemashi1@gmail.com>
2020-10-31 14:36:15 +0900

    タイマー追加と軌道変更可にした

---
+++ Assets/Scripts/Seya/SeyaTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeyaTimer : MonoBehaviour
+{
+    [SerializeField] float limitTime = 1.0f;
+    private void Start()
+    {
+        StartCoroutine("Timer");
+    }
+    IEnumerator Timer()
+    {
+        while (limitTime >= 0)
+        {
+            Debug.Log(limitTime);
+            yield return new WaitForSeconds(1.0f);
+            limitTime -= 1.0f;
+        }
+    }
+}
