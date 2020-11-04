a98ef211bec39ab3de02d04ae24f6ad9c8612d1f
teruteru634 <miyanagateru634@gmail.com>
2020-11-04 19:27:52 +0900

    t

---
+++ Assets/Scripts/Musahi/Core/GameBaseMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBaseMain : MonoBehaviour
+{
+    [SerializeField] VolumeConfigUI m_volumeConfig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_volumeConfig.SetMaseterSliderEvent(v => SoundManager.Instance.MasterVolume = v);
+    }
+}
