cf4780ca06c31373a8bb62700bd899d7c9c65924
sorasemashi <sorasemashi1@gmail.com>
2020-11-06 16:39:48 +0900

    Merge branch 'main' of https://github.com/M-634/BallonGame into seya
    # Conflicts:
    #	Assets/Scenes/ProtoType/Test300-400.unity

---
+++ Assets/Scripts/Musahi/Test/Test_Player_Musashi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Player_Musashi : MonoBehaviour
+{
+    TimerInStage m_gameState;
+    public float m_speed = 1f;
+
+    EventSystemInGameScene m_eventSystemInGameScene;
+    private void Awake()
+    {
+        //ここもっといいやり方ないかなー|дﾟ)
+        m_gameState = FindObjectOfType<TimerInStage>();
+        if (m_gameState == null)
+        {
+            Debug.LogError("TimeSchedulerコンポーネントをアタッチされたゲームオブジェクトが存在しません");
+        }
+        m_eventSystemInGameScene = FindObjectOfType<EventSystemInGameScene>();
+       if (m_eventSystemInGameScene == null)
+        {
+            Debug.LogError("EventSystemInGameSceneコンポーネントがアタッチされたゲームオブジェクトが存在しません");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_gameState.InGame) return;
+
+        transform.position += new Vector3(0, 0, m_speed / 90f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<IEventCollision>(out var eventCollision))
+        {
+            eventCollision.CollisionEvent(m_eventSystemInGameScene);
+        }
+    }
+}
