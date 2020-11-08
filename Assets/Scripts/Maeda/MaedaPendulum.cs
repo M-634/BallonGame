95b6fe852a9beb85080890e065389082c2a5ed99
masabonoboTaisa <mauwarote@gmail.com>
2020-11-09 01:22:27 +0900

    振り子をより扱いやすくした（動きはまだ少し変）

--- Assets/Scripts/Maeda/MaedaPendulum.cs
+++ Assets/Scripts/Maeda/MaedaPendulum.cs
@@ -3,58 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEditor;
+using System.Linq;
 
 public class MaedaPendulum : MonoBehaviour
 {
     [SerializeField] GameObject pivot;
-    private float radius = 1.5f;
-    private bool isPlus;
-    [SerializeField] float speed;
+    [Tooltip("0∼0.5の間で")]
+    [SerializeField] float m_rad;
+    [Tooltip("4以上で")]
+    [SerializeField] int pathVertex;
+    [Tooltip("往復にかかる秒数")]
     [SerializeField] float interval;
     [Tooltip("X座標の振幅")]
     [SerializeField] float x_amplitude;
     [Tooltip("Y座標の振幅")]
     [SerializeField] float y_amplitude;
 
-    private void Start()
+    private void Awake()
     {
+        float rad = (2 * m_rad) / pathVertex;
+        float radNatureNum = 1.5f - m_rad;
+        List<Vector3> vector3s = new List<Vector3>();
+        for (int i = 0; i < pathVertex; i++)
+        {
+            float radius = Mathf.PI * radNatureNum;
+            Vector3 vertex = new Vector3(Mathf.Cos(radius) * x_amplitude + transform.position.x, Mathf.Sin(radius) * y_amplitude + transform.position.y, transform.position.z);
+            vector3s.Add(vertex);
+            radNatureNum += rad;
+        }
+        for (int i = vector3s.Count - 2; i > 0; i--)
+        {
+            vector3s.Add(vector3s[i]);
+        }
+        vector3s.RemoveAt(0);
+        Vector3[] vecArray = vector3s.ToArray();
         DOTween.Sequence()
-            .Join(transform.DOMove(GetPendulumTransform(), 0))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
-            .Append(transform.DOMove(GetPendulumTransform(), interval))
+            .Append(transform.DOMove(vector3s[0], 0))
+            .Append(transform.DOPath(vecArray, interval, PathType.CatmullRom).SetEase(Ease.InOutQuart))
             .SetLoops(-1)
             .Play();
-    }
-
-    Vector3 GetPendulumTransform()
-    {
-        Vector3 vector = new Vector3(pivot.transform.position.x + Mathf.Cos(Mathf.PI * radius) * x_amplitude, pivot.transform.position.y + Mathf.Sin(Mathf.PI * radius) * y_amplitude, pivot.transform.position.z);
-        if (isPlus)
-        {
-            radius += speed;
-            if (radius > 1.75f)
-            {
-                isPlus = false;
-            }
-        }
-        else
-        {
-            radius -= speed;
-            if (radius < 1.25f)
-            {
-                isPlus = true;
-            }
-        }
-        return vector;
     }
 }
