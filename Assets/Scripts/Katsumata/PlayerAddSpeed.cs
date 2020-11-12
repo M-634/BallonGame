2bff93d2c13db2abe8cdac3c312575625ab45d4a
Katsumata <dai1nokira@gmail.com>
2020-11-12 22:41:54 +0900

    加速床はtime.deltaTimeで時間を測って一定間隔で加速するようにした

--- Assets/Scripts/Katsumata/PlayerAddSpeed.cs
+++ Assets/Scripts/Katsumata/PlayerAddSpeed.cs
@@ -12,31 +12,36 @@ public class PlayerAddSpeed : MonoBehaviour
     /// <summary>加速する係数 </summary>
     [SerializeField] float addCoefficient = 1500;
     /// <summary>次に加速が始まるまでの待機時間 </summary>
-    [SerializeField] int accelWaitTime = 2500;
-    bool onWaitAccel = false;
+    [SerializeField] float accelWaitTime = 2.5f;
+    bool onAccelable = true;
+    float waitTime = 0;
 
     private void Start()
     {
         m_rb = GetComponent<Rigidbody>();
 
     }
+
+    private void Update()
+    {
+        if (!onAccelable)
+        {
+            waitTime += Time.deltaTime;
+            onAccelable = waitTime > accelWaitTime ? true : false;
+        }
+        else
+        {
+            waitTime = 0;
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "acceleration" && !onWaitAccel)
+        if (other.tag == "acceleration" && onAccelable)
         {
-            m_rb.AddForce(this.transform.forward * addCoefficient);
-            onWaitAccel = true;
-            var task = DelayAsync().ContinueWith(t => onWaitAccel = false);
-            
+            m_rb.AddForce(transform.forward * addCoefficient);
+            onAccelable = false;
         }
     }
 
-    async Task DelayAsync()
-    {
-        await Task.Delay(accelWaitTime);
-        Debug.Log("加速待ち");
-        
-        //return onWaitAccel = false;
-    }
 
 }
