15b8a5d5fbf191a093d2d56735bc5c9dd1514fc6
K_Arai00 <attckonk@gmail.com>
2020-11-27 18:44:12 +0900

    動く壁のちょっとした変更、動く壁ではなくScaleが変わるObjectの作成

--- Assets/Scripts/AraiKanta/Arai_MoveWall.cs
+++ Assets/Scripts/AraiKanta/Arai_MoveWall.cs
@@ -14,15 +14,40 @@ public class Arai_MoveWall : MonoBehaviour
     [SerializeField] private float endPosX;
     /// <summary>this.transform.DOMoveの移動時間の変数</summary>
     [SerializeField] private float traveTime;
-    /// <summary>this.transform.DOMoveXの遅延時間の変数</summary>
+    /// <summary>this.transform.DOMoveX & DOScale の遅延時間の変数</summary>
     [SerializeField] private float delayTime;
+    /// <summary>this.transform.DOScaleのXscaleの変数</summary>
+    [SerializeField] private float xScale;
+    /// <summary>this.transform.DOScaleのXscaleの変数</summary>
+    [SerializeField] private float x1Scale;
+
+    /// <summary>動く壁</summary>
+    [SerializeField] bool moveWall;
+    /// <summary>大きさ変わる壁</summary>
+    [SerializeField] bool scale;
 
     void Start()
     {
-        DOTween.Sequence()
+        //動く壁
+        if (moveWall)
+        {
+            DOTween.Sequence()
             .Append(this.transform.DOMoveX(startPosX, traveTime).SetDelay(delayTime))
             .Append(this.transform.DOMoveX(endPosX, traveTime).SetDelay(delayTime))
             .SetLoops(-1)
             .Play();
+        }
+
+        //一応Sucaleを変更できるように
+        if (scale)
+        {
+            this.transform.localScale = new Vector3(0f, transform.localScale.y, transform.localScale.z);
+
+            DOTween.Sequence()
+                .Append(this.transform.DOScaleX(endValue: xScale, duration: traveTime).SetDelay(delayTime))
+                .Append(this.transform.DOScaleX(endValue: x1Scale, duration: traveTime).SetDelay(delayTime))
+                .SetLoops(-1)
+                .Play();
+        }
     }
 }
