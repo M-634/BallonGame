763c0a0785f5ae105a89051eb16ac1a9f9ff7143
teruteru634 <miyanagateru634@gmail.com>
2020-12-14 20:33:51 +0900

    PlayerEventHandllerの不具合を修正した

--- Assets/Scripts/Musashi/Core/PlayerEventHandller.cs
+++ Assets/Scripts/Musashi/Core/PlayerEventHandller.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*********
+*fileをScripts > Mushashiに移動しました。
+*ここはムサシ以外にいじるな！
+*********/
 
-public class PlayerEventHandller : EventReceiver<Test_Player_Musashi>
+/// <summary>
+/// プレイヤーがゲームシーン上のイベントを購読するクラス
+/// </summary>
+public class PlayerEventHandller : EventReceiver<PlayerEventHandller>
 {
     public bool InGame { get; set; }
 
-    //TO DO:川嶋が加えたよ playerの動きだけをテストしたい時にチェックをいれてね
+    [Header("playerの動きだけをテストしたい時にチェックをいれてね")]
     [SerializeField] bool m_doDebugPlayerMove;
-    ForcePlayerMove m_playerMove;
+    Rigidbody m_rb;
 
     protected override void Awake()
     {
         if (m_doDebugPlayerMove) return;
-        m_playerMove = GetComponent<ForcePlayerMove>();
-        m_playerMove.m_rb.constraints = RigidbodyConstraints.FreezePosition;
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.constraints = RigidbodyConstraints.FreezePosition;
         base.Awake();
     }
-    
+
 
     /// <summary>
     /// 障害物とか
@@ -45,15 +52,15 @@ public class PlayerEventHandller : EventReceiver<Test_Player_Musashi>
 
     public void GameStart()
     {
-        m_playerMove.m_rb.constraints = RigidbodyConstraints.None;
+        m_rb.constraints = RigidbodyConstraints.None;
         InGame = true;
-        
+
     }
 
     public void EndGaem()
     {
         InGame = false;
-        GetComponent<PlayerBaseMove>().StopMove();
+        m_rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     protected override void OnEnable()
--- Assets/Scripts/Musashi/UI/LoadSceneButtonInSelect.cs
+++ Assets/Scripts/Musashi/UI/LoadSceneButtonInSelect.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
-/// ロードボタン用のスクリプト
+/// セレクトシーン内のロードボタン用のスクリプト
 /// </summary>
 public class LoadSceneButtonInSelect : MonoBehaviour
 {
+    [SerializeField] Text m_stageNameText;
+
     public void LoadTitleScene()
     {
         SceneLoader.Instance.LoadTitleScene();
@@ -13,6 +16,12 @@ public class LoadSceneButtonInSelect : MonoBehaviour
 
     public void LoadGameScene()
     {
+        if (StageParent.Instance.GetAppearanceStageData == null
+            || StageParent.Instance.GetAppearanceStagePrefab == null)
+        {
+            m_stageNameText.text = "Stageを選択してね！！";
+            return;
+        }
         SceneLoader.Instance.LoadGameScene();
     }
 }
