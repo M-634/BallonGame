a6947146c425def3ab39ea81f974e6580a77ee50
Katsumata <dai1nokira@gmail.com>
2020-12-22 11:00:55 +0900

    AddSpeedGimicシーン、追い風、気流スクリプトの無駄なものを削除した

--- Assets/Scripts/Katsumata/PlayerController/Gimic/PlayerOnStream.cs
+++ Assets/Scripts/Katsumata/PlayerController/Gimic/PlayerOnStream.cs
@@ -8,30 +8,14 @@ using DG.Tweening;
 /// </summary>
 public class PlayerOnStream : MonoBehaviour
 {
-    //Rigidbody m_rb;
-    //[Header("追加の加速度")]
-    ///// <summary>加速する係数 </summary>
-    //[SerializeField] float m_addCoefficient = 50;
-
-    //[Header("加速ギミック(気流)を抜けた直後のy軸空気摩擦")]
-    ///// <summary>加速ギミックを抜けた後のy軸空気摩擦 </summary>
-    //[SerializeField] float m_afterAccelDrag = 0.9f;
-
-    [Header("加速ギミック(気流)後のy軸空気摩擦のかかる時間")]
     /// <summary>加速ギミックを抜けた後の特別な空気摩擦が生じる時間 </summary>
+    [Header("加速ギミック(気流)後のy軸空気摩擦のかかる時間")]
     [SerializeField] float m_moveTime = 1.0f;
 
-    //[Header("加速ギミックを抜けた後のY軸の速さ")]
-    ///// <summary>
-    ///// 加速ギミックを抜けた後のY軸の速さ
-    ///// m_afterAccelDragTimeの時間をかけてm_afterAccelDragの力をかけてこの速さにする
-    ///// </summary>
-    //[SerializeField] Vector3 m_afterAccelSpeed;
-
-    [Header("上がる又は下がる高さ")]
     /// <summary>
     /// 上がる又は下がる高さ
     /// </summary>
+    [Header("上がる又は下がる高さ")]
     [SerializeField] float amountMovement = 20.0f;
 
     [Header("動くときの緩急")]
@@ -53,41 +37,14 @@ public class PlayerOnStream : MonoBehaviour
         }
 
     }
-    private void OnTriggerStay(Collider other)
-    {
-        //if (other.tag == "acceleration" && m_rb.velocity.magnitude < accelMaxSpeed)
-        //{
-        //    m_rb.AddForce(other.transform.forward * m_addCoefficient); //ローカル座標でのforward
-        //}
-
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        //if (other.tag == "acceleration")
-        //{
-        //    //m_onHeightDrag = true;
-        //    cameraMove.ChangeBaseCamera();
-
-        //}
-    }
 
     void MovePoint(GameObject player)
     {
-        //var currentPosition = transform.position;
         Debug.Log("MovePoint!");
         player.transform.DOLocalMoveY(amountMovement, m_moveTime)
             .SetRelative(true)
             .SetEase(moveMethod)
             .OnComplete(() => cameraMove.ChangeBaseCamera())
             .OnComplete(() => m_onDGMove = false);
-        //DOTween.To(
-        //    () => currentPosition,
-        //    (x) => transform.position = x,
-        //    moveGoalPosi,
-        //    m_moveTime)
-        //    .SetEase(moveMethod)
-        //    .OnComplete(() => cameraMove.ChangeBaseCamera())
-        //    .OnComplete(() => m_onDGMove = false);
     }
 }
