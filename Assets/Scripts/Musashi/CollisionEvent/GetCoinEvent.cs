4eea8cf42a7d6c74af907395db91675479590d74
teruteru634 <miyanagateru634@gmail.com>
2021-01-13 17:15:52 +0900

    コインの回転

--- Assets/Scripts/Musashi/CollisionEvent/GetCoinEvent.cs
+++ Assets/Scripts/Musashi/CollisionEvent/GetCoinEvent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
 /// <summary>
 /// コイン獲得時のイベント
@@ -11,6 +12,14 @@ public class GetCoinEvent : MonoBehaviour, IEventCollision
     /// <summary>コイン獲得時のスコアを設定してね</summary>
     [SerializeField] int m_getScore;
     [SerializeField] string m_getCoinClipName;
+
+    private void Start()
+    {
+        //アクティブがfalseに回転し続けるからどうにかしたいがDoTweenがうまく動いてくれないので後回し
+        transform.DOLocalRotate(Vector3.up * 360, Random.Range(1f, 2f), RotateMode.FastBeyond360)
+             .SetEase(Ease.Linear)
+             .SetLoops(-1, LoopType.Restart);
+    }
 
     public void CollisionEvent(EventSystemInGameScene eventSystem)
     {
