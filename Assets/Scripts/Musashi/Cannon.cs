63c3b58521f61d3cc80ebc2d3e54e9af365ab698
teruteru634 <miyanagateru634@gmail.com>
2021-01-14 17:44:08 +0900

    Cannonを修正した

---
+++ Assets/Scripts/Musashi/Cannon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField] CannonBullet[] m_bullets;
+    [SerializeField] Transform m_muzzlePos;
+    [SerializeField] float m_shootPower;
+    [SerializeField] float m_coolTime;
+    int m_index = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var target = GameObject.FindGameObjectWithTag("CannonTarget").transform;
+            var dir = target.position - m_muzzlePos.position;
+            //plyaerの方へむく
+            dir.y = 0;
+            var lookRotation = Quaternion.LookRotation(dir * -1, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 1f);
+            //発射
+            m_bullets[m_index].Shoot(dir.normalized * m_shootPower);
+        }
+    }
+}
---
+++ Assets/Scripts/Musashi/CannonBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonBullet : MonoBehaviour
+{
+    Rigidbody m_rb;
+    Vector3 m_firstpos;
+
+    private void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_firstpos = transform.position;
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        gameObject.SetActive(true);
+        m_rb.AddForce(dir);
+        StartCoroutine(gameObject.SetActive(false,10f));
+    }
+
+    public void OnDisable()
+    {
+        transform.position = m_firstpos; 
+    }
+
+}
+
+public static class ExtendGameObjectClass
+{
+    public static IEnumerator SetActive(this GameObject gameObject,bool value,float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        gameObject.SetActive(value);
+    }
+}
