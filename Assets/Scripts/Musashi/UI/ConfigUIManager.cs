340c1f1add021e6d162f7e8a1f8027d659964d17
teruteru634 <miyanagateru634@gmail.com>
2021-01-15 05:45:40 +0900

    とりあえず、全ての実装は終わった。細かい調整は後回し。実機テストする

--- Assets/Scripts/Musashi/UI/ConfigUIManager.cs
+++ Assets/Scripts/Musashi/UI/ConfigUIManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 /// <summary>
 /// 設定画面を管理するクラス
 /// </summary>
-public class ConfigUIManager :SingletonMonoBehavior<ConfigUIManager> 
+public class ConfigUIManager : SingletonMonoBehavior<ConfigUIManager>
 {
     [SerializeField] string m_audioClipName;
+    [SerializeField] Button m_retryBtn;
     private CanvasGroup m_canvasGroup;
+    private Scene m_currentScene;
 
     protected override void Awake()
     {
@@ -26,15 +30,47 @@ public class ConfigUIManager :SingletonMonoBehavior<ConfigUIManager>
         SoundManager.Instance.PlayMenuSe(m_audioClipName);
         m_canvasGroup.Show();
         //ゲームシーンなら一時停止する
-        Time.timeScale = 0f;
-        SoundManager.Instance.Pause();
+        m_currentScene = SceneManager.GetActiveScene();
+        if (m_currentScene.buildIndex == 2)
+        {
+            m_retryBtn.interactable = true;
+            Time.timeScale = 0f;
+            SoundManager.Instance.Pause();
+        }
+        else
+        {
+            m_retryBtn.interactable = false;
+        }
     }
 
     public void CloseCnfigUI()
     {
         SoundManager.Instance.PlayMenuSe(m_audioClipName);
         m_canvasGroup.Hide();
+        if (m_currentScene.buildIndex == 2)
+        {
+            Time.timeScale = 1f;
+            SoundManager.Instance.Resume();
+        }
+    }
+
+    public void PressRetry()
+    {
+        SoundManager.Instance.PlayMenuSe(m_audioClipName);
+        m_canvasGroup.Hide();
         Time.timeScale = 1f;
-        SoundManager.Instance.Resume();
+        SoundManager.Instance.ReStart();
+        SceneManager.LoadScene(m_currentScene.name);
+    }
+
+    public void QuitApplication()
+    {
+        SoundManager.Instance.PlayMenuSe(m_audioClipName);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_ANDROID
+        Application.runInBackground = false;
+        Application.Quit();
+#endif
     }
 }
